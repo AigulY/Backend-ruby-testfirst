fa747303dcddbb65c9cb7475c290eac6325e397b
AigulY <azia.kz.a@gmail.com>
2023-11-20 13:08:06 -0500

    testfish-lesson

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(array)
+    sum = 0
+    array.each do |number|
+        sum += number
+    end
+    return sum
+end
+
+def multiply(a, b)
+    return 0 if a == 0 || b == 0
+
+    return a * b
+end
+
+def power (a, b)
+    return 1 if b == 0
+
+    result = a
+    (b - 1).times do 
+         sum *= a
+    end
+    return result
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    exceptions = %w[and the a an at to on in of]
+    def title=(new_title)
+      @title = new_title.split.map.with_index do |word, index|
+        if index.zero? || !exceptions.include?(word.downcase)
+          word.capitalize
+        else
+          word
+        end
+      end.join(" ")
+    end
 end
